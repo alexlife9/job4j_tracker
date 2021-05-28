97ccd70c432e10bc79a8f9ef6b44218f55464959
Alexlife9 <crashmx@inbox.ru>
2021-05-28 13:12:31 +0300

    3.1. Массив с пустыми ячейками [#176470 #161452]

--- src/test/java/ru/job4j/pojo/ShopTest.java
+++ src/test/java/ru/job4j/pojo/ShopTest.java
@@ -9,7 +9,7 @@ public class ShopTest {
     @Test
     public void whenLastNull() {
         Product[] products = new Product[5];
-        products[0] = new Product("Milk", 10);
+        products[0] = new Product("Milk", 9);
         products[1] = new Product("Bread", 4);
         products[2] = new Product("Egg", 19);
         int rsl = Shop.indexOfNull(products);
