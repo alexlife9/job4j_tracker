a12d140c79f4cfc331b7269f01a9ad0d607831f1
Alexlife9 <crashmx@inbox.ru>
2021-06-04 11:27:43 +0300

    4.1. Разрыв зависимости StartUI от Scanner

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -23,7 +23,7 @@ public class StartUI {
 
     public static void replaceItem(Input input, Tracker tracker) {
         System.out.println("=== Edit item ====");
-        int id = Integer.parseInt(input.askStr("Enter ID item: "));
+        int id = input.askInt("Enter ID item: ");
         String name = input.askStr("Enter new name: ");
         Item item = new Item(name);
         if (tracker.replace(id, item)) {
@@ -35,7 +35,7 @@ public class StartUI {
 
     public static void deteleItem(Input input, Tracker tracker) {
             System.out.println("=== Delete item ====");
-            int id = Integer.parseInt(input.askStr("Enter ID item: "));
+            int id = input.askInt("Enter ID item: ");
             if (tracker.delete(id)) {
                 System.out.println("Successfully! Item with ID" + id + " delete");
             } else {
@@ -45,7 +45,7 @@ public class StartUI {
 
     public static void findIdItem(Input input, Tracker tracker) {
         System.out.println("=== Find item by Id ====");
-        int id = Integer.parseInt(input.askStr("Enter ID item: "));
+        int id = input.askInt("Enter ID item: ");
         Item item = tracker.findById(id);
         if (item != null) {
             System.out.println(item);
@@ -71,7 +71,7 @@ public class StartUI {
         boolean run = true;
         while (run) {
             showMenu();
-            int select = Integer.parseInt(input.askStr("Select: "));
+            int select = input.askInt("Select: ");
             if (select == 0) {
                 StartUI.createItem(input, tracker);
 
@@ -116,11 +116,5 @@ public class StartUI {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         new StartUI().init(input, tracker);
-        StartUI.createItem(input, tracker);
-        StartUI.allItem(input, tracker);
-        StartUI.replaceItem(input, tracker);
-        StartUI.deteleItem(input, tracker);
-        StartUI.findIdItem(input, tracker);
-        StartUI.findNameItem(input, tracker);
     }
 }
