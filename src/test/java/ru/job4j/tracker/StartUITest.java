fbdd2c26eff81b7a8c5fae877b7324132218944f
Alexlife9 <crashmx@inbox.ru>
2021-06-09 20:41:33 +0300

    9.2. Тесты вывода на консоль

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -11,23 +11,39 @@ public class StartUITest {
     public void whenAllItem() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"0", "1"}
-
+                new String[] {"0", "Test1", "1"}
         );
         Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Test1"));
         UserAction[] actions = {
-                new AllAction(out), new ExitAction(out)
+                new CreateAction(out),
+                new AllAction(out),
+                new ExitAction(out)
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
-                "Menu." + System.lineSeparator()
-                        + "0. Show all items" + System.lineSeparator()
+                "Menu:" + System.lineSeparator()
+                        + "0. Add new Item" + System.lineSeparator()
+                        + "=== Create a new Item ====" + System.lineSeparator()
+                        + "Enter name: " + System.lineSeparator()
+                        + item + System.lineSeparator()
+                        + "Menu:" + System.lineSeparator()
+                        + "1. === Show all items ====" + System.lineSeparator()
                         + "1. Exit Program" + System.lineSeparator()
         ));
     }
 
     @Test
     public void whenFindByName() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0", "test1", "1"}
+
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new FindNameAction(out), new ExitAction(out)
+        };
 
     }
 
@@ -48,7 +64,7 @@ public class StartUITest {
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
-                "Menu." + System.lineSeparator()
+                "Menu:" + System.lineSeparator()
                         + "0. Exit Program" + System.lineSeparator()
         ));
     }
