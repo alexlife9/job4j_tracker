183ee398947d6e0c62e2a153d95f183d758bff81
Alexlife9 <crashmx@inbox.ru>
2021-08-03 00:31:10 +0300

    2. Преобразования List в Map

--- src/test/java/ru/job4j/collection/OrderConvertTest.java
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -17,4 +17,15 @@ public class OrderConvertTest {
         HashMap<String, Order> map = OrderConvert.process(orders);
         assertThat(map.get("3sfe"), is(new Order("3sfe", "Dress")));
     }
+
+    @Test
+    public void whenDuplicateOrder() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("1", "Dress"));
+        orders.add(new Order("1","Face"));
+        orders.add(new Order("2", "Code"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        assertThat(map.get("1"), is(new Order("1", "Face")));
+        assertThat(map.get("2"), is(new Order("2", "Code")));
+    }
 }
