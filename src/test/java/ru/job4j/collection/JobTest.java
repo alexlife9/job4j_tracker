0bca2cb0ed4dce417d27b24e58cd1125979c6c45
Alexlife9 <crashmx@inbox.ru>
2021-08-12 23:53:41 +0300

    2. Комбинированный компаратор

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -10,8 +10,8 @@ public class JobTest {
     public void whenCompatorByNameAndPrority() {
         Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 0)
         );
         assertThat(rsl, lessThan(0));
     }
