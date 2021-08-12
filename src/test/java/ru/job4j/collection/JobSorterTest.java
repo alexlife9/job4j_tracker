468c9a0c98b3a197d9b4b26ed09fafea333a12f4
Alexlife9 <crashmx@inbox.ru>
2021-08-12 12:35:36 +0300

    2. Комбинированный компаратор

--- src/test/java/ru/job4j/collection/JobSorterTest.java
+++ src/test/java/ru/job4j/collection/JobSorterTest.java
@@ -3,8 +3,8 @@ package ru.job4j.collection;
 import org.junit.Test;
 import java.util.Comparator;
 
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class JobSorterTest {
@@ -15,21 +15,31 @@ public class JobSorterTest {
                 new Job("Fix bug", 2),
                 new Job("Reboot server", 4)
         );
-        assertThat(rsl, greaterThan(2));
+        assertThat(rsl, greaterThan(1));
     }
 
     @Test
     public void whenJobByNameUp() {
         Comparator<Job> cmpJobNameUp = new JobByNameUp();
         int rsl = cmpJobNameUp.compare(
-                new Job("Reboot bug", 2),
-                new Job("Fix server", 3)
+                new Job("Fix bug", 2),
+                new Job("Reboot server", 3)
         );
-        assertThat(rsl, greaterThan(0));
+        assertThat(rsl, lessThan(1));
     }
 
     @Test
-    public void whenJobByPriorityDawn() {
+    public void whenJobByNameEqual() {
+        Comparator<Job> cmpJobNameUp = new JobByNameUp();
+        int rsl = cmpJobNameUp.compare(
+                new Job("bug", 2),
+                new Job("bug", 3)
+        );
+        assertThat(rsl, is(0));
+    }
+
+    @Test
+    public void whenJobByPriorityDown() {
         Comparator<Job> cmpJobPriorityDown = new JobByPriorityDown();
         int rsl = cmpJobPriorityDown.compare(
                 new Job("Reboot bug", 4),
@@ -45,6 +55,17 @@ public class JobSorterTest {
                 new Job("Reboot bug", 7),
                 new Job("Fix server", 5)
         );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenJobByNameAndPriority() {
+        Job o1 = new Job("Reboot bug", 0);
+        Job o2 = new Job("Fix server", 1);
+        Comparator<Job> cmpJobNameAndPriority = new JobDescByNameLn().thenComparing(new JobDescByPriority());
+        Comparator<Job> cmpDescByNameLn = new JobDescByNameLn();
+        int rsl = cmpJobNameAndPriority.compare(o1, o2);
+        assertEquals(0, cmpDescByNameLn.compare(o1, o2));
         assertThat(rsl, greaterThan(0));
     }
 }
