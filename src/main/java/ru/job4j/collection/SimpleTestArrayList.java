cf121be1cb4099bc7d83e8ed74b9560df56128d5
Alexlife9 <crashmx@inbox.ru>
2021-08-20 09:54:52 +0300

    вывод списка ArrayList

---
+++ src/main/java/ru/job4j/collection/SimpleTestArrayList.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class SimpleTestArrayList {
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("test1");
+        list.add("test2");
+        list.add("test3");
+        System.out.print(list.get(1) + ":");
+        list.add(1, "test4");
+        System.out.print(list.get(1) + ":");
+        for (String s : list) {
+            System.out.print(s + ":");
+        }
+    }
+}
+
+/*
+  Элементы в ArrayList нумеруются начиная с нуля. Поэтому элемент с номером 1 — это test2.
+  Следующим действием мы добавляем строку «test4» в ячейку с индексом 1.
+  При этом элементы с бо́льшим индексом сдвигаются вправо.
+  Вторая часть вывода (test4) показывает, что теперь по индексу 1 извлекается именно test4.
+  Далее мы обходим все элементы списка и убеждаемся, что они выводятся именно в порядке добавления.
+ */
