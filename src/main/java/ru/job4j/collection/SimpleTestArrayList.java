5adaa17cc1fef58860081f9bcb568c3eb168f679
Alexlife9 <crashmx@inbox.ru>
2021-08-20 11:13:28 +0300

    вывод списка ArrayList

--- src/main/java/ru/job4j/collection/SimpleTestArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleTestArrayList.java
@@ -6,15 +6,28 @@ public class SimpleTestArrayList {
 
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
-        list.add("test1");
-        list.add("test2");
-        list.add("test3");
-        System.out.print(list.get(1) + ":");
-        list.add(1, "test4");
-        System.out.print(list.get(1) + ":");
-        for (String s : list) {
-            System.out.print(s + ":");
-        }
+//        list.add("test1");
+//        list.add("test2");
+//        list.add("test3");
+//        System.out.print(list.get(1) + ":");
+//        list.add(1, "test4");
+//        System.out.print(list.get(1) + ":");
+//        for (String s : list) {
+//            System.out.print(s + ":");
+//        }
+        String t1 = "test1";
+        String t2 = "test2";
+        list.add(t1);
+        list.add(t2);
+        System.out.println(list.size() + " - " + t1 + ":" + t2);
+        t1 = "test3";
+        System.out.println(list.size() + " - " + t1 + ":" + t2);
+        list.remove(t1);
+        System.out.println(list.size() + " - " + t1 + ":" + t2);
+        list.remove("test1");
+        System.out.println(list.size() + " - " + t1 + ":" + t2);
+        list.remove("test2");
+        System.out.println(list.size() + " - " + t1 + ":" + t2);
     }
 }
 
