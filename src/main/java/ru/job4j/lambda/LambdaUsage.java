b6ba47d1bbb84c70866503188970bbade9ee64ea
Alexlife9 <crashmx@inbox.ru>
2021-08-30 10:12:04 +0300

    2.2. Лямбда блок

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,23 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collector;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("ааа");
+        list.add("а");
+        list.add("аа");
+        list.add("aaaa");
+        System.out.println("До сортировки: " + list);
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + left.length() + " : " + right.length());
+            return Integer.compare(right.length(), left.length());
+        };
+        list.sort(cmpDescSize);
+        System.out.println("После сортировки: " + list);
+    }
+}
