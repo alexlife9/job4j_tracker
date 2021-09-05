f925a8a58e6e93b6555d31c0e506f2f27604c7d7
Alexlife9 <crashmx@inbox.ru>
2021-09-05 22:08:12 +0300

    3. Уникальность элементов и сортировка

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -23,4 +23,19 @@ public class ProfilesTest {
         List<Address> expected = Arrays.asList(alex, oleg, lena);
         assertThat(result, is(expected));
     }
+
+    @Test
+    public void collectTestUnicArray() {
+        Address alex = new Address("EKB", "Lenina", 13, 28);
+        Address oleg = new Address("SPB", "Fontanka", 9, 75);
+        Address lena = new Address("EKB", "Lenina", 13, 28);
+        List<Profile> prof = Arrays.asList(
+                new Profile(alex),
+                new Profile(oleg),
+                new Profile(lena)
+        );
+        List<Address> result = Profiles.collectUnic(prof);
+        List<Address> expected = Arrays.asList(alex, oleg);
+        assertThat(result, is(expected));
+    }
 }
