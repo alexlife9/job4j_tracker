66551dbb71089fc567ddaf4029b2e2ddacab8362
Alexlife9 <crashmx@inbox.ru>
2023-09-22 02:46:17 -0300

    Реализовать элементарный алгоритм сжатия строки на основе счетчика повторяющихся символов

---
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -0,0 +1,40 @@
+package ru.job4j.array;
+
+/**
+ * Реализовать элементарный алгоритм сжатия строки на основе счетчика повторяющихся символов
+ *
+ * @author Alex_life
+ * @version 1.0
+ * @since 22.09.2023
+ */
+public class SimpleStringEncoder {
+    public static String encode(String input) {
+        if (input == null || input.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder compress = new StringBuilder();
+        char symbol = input.charAt(0);
+        int counter = 1;
+
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == symbol) {
+                counter++;
+            } else {
+                compress.append(symbol);
+                if (counter > 1) {
+                    compress.append(counter);
+                }
+                symbol = input.charAt(i);
+                counter = 1;
+            }
+        }
+
+        compress.append(symbol);
+        if (counter > 1) {
+            compress.append(counter);
+        }
+
+        return compress.toString();
+    }
+}
